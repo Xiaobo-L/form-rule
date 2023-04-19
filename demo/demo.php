18575f4ca1824e6d7b00fff42f5c72de618706a2
xiaobo <1263585140@qq.com>
2023-04-19 13:35:44 +0800

    main

---
+++ demo/demo.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use FormRule\Form;
+use FormRule\FormUtil;
+
+require '../vendor/autoload.php';
+
+
+
+$f = array();
+$f[] = Form::input('nickname', '')->col(4)->placeholder('名称');
+$f[] = Form::input('nickname', '姓名')->appendRule('validate', [[
+    'type' => 'string',
+    'required' => true,
+    'message' => '请输入名称'
+]]);
+$f[] = Form::password('password', '密码')->appendRule('validate', [[
+    'type' => 'string',
+    'required' => true,
+    'pattern' => '[a-zA-Z0-9_-]{4,16}',
+    'message' => '用户名正则，4到16位（字母，数字，下划线，减号）'
+]]);;
+$f[] = Form::uploadImage('avatar', '头像', '控制器');
+$from = Form::elm_get_form($f);
+
+exit(json_encode(FormUtil::jsonSuccess($from)));
